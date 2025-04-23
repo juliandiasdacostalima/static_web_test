58308f75b79d629668993d7c8364b6e8b1b72fb8
juliandiasdacostalima <103572225+juliandiasdacostalima@users.noreply.github.com>
2025-04-23 18:21:43 +0200

    Update __init__.py

--- api/saludar/__init__.py
+++ api/saludar/__init__.py
@@ -1,8 +1,17 @@
+import logging
+import azure.functions as func
 import requests
+import json
 
-def main(req):
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": {"mensaje": "Hola desde Python con requests!"}
-    }
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Función "saludar" llamada.')
+
+    # Llamado ficticio para demostrar uso de requests
+    r = requests.get("https://api.chucknorris.io/jokes/random")
+    data = r.json()
+
+    return func.HttpResponse(
+        json.dumps({"mensaje": f"Hola Julian! Chiste: {data['value']}"}),
+        mimetype="application/json",
+        status_code=200
+    )
